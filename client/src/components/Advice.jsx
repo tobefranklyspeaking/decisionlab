04116b52983b821488efa9c351edd6b408b6b757
Frank G <fgauss3@gmail.com>
2021-09-08 15:10:36 -0400

    refactored Advice as functional component

--- client/src/components/Advice.jsx
+++ client/src/components/Advice.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
 
-class Advice extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render() {
-    return (
-      <div className='finalAdvice'>
-        <div>1. Don’t stress</div>
-        <div>2. Take some time to reflect</div>
-        <div>3. Break down decisions into smaller pieces</div>
-        <div>4. Consider options</div>
-        <div>5. Write it out</div>
-        <div>6. Pros/Cons list</div>
-        <div>7. Talk it out</div>
-        <div>8. Use this app</div>
-        <a href='https://psychcentral.com/blog/15-tips-to-help-you-make-the-most-important-decisions#1' target='_blank'>Need more? Click me</a>
-      </div>
-    )
-  }
+const Advice = () => {
+  return (
+    <div className='finalAdvice'>
+      <div>1. Don’t stress</div>
+      <div>2. Take some time to reflect</div>
+      <div>3. Break down decisions into smaller pieces</div>
+      <div>4. Consider options</div>
+      <div>5. Write it out</div>
+      <div>6. Pros/Cons list</div>
+      <div>7. Talk it out</div>
+      <div>8. Use this app</div>
+      <a href='https://psychcentral.com/blog/15-tips-to-help-you-make-the-most-important-decisions#1' target='_blank'>Need more? Click me</a>
+    </div>
+  )
 }
 
 export default Advice;
